aca91f006add63ad417ec53df733b7c87239d0f8
SaltBecon <163530950+SaltBecon@users.noreply.github.com>
2024-08-03 03:42:30 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const inputTable = document.getElementById("input_table");
 const colors = ["purple", "sky_blue", "yellow", "red", "purplish_red", "blue"];
 const options = ["紫色(2重線)", "水色", "黄色", "赤色", "赤紫色(太い点線)", "青色(丸付きの線)"];
 let ringCount = 0;
-addRing();
 
 function addRing() {
     let tr = document.createElement("tr");
@@ -27,4 +26,7 @@ function addRing() {
     tr.appendChild(number);
     tr.appendChild(range);
     inputTable.appendChild(tr);
+    ringCount++;
 }
+
+addRing();
