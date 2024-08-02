77f620d9b39db8091dc4cba4fef0017ef42d976a
SaltBecon <163530950+SaltBecon@users.noreply.github.com>
2024-08-03 03:25:21 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const inputTable = document.getElementById("input_table");
+const colors = ["purple", "sky_blue", "yellow", "red", "purplish_red", "blue"];
+const options = ["紫色(2重線)", "水色", "黄色", "赤色", "赤紫色(太い点線)", "青色(丸付きの線)"];
 let ringCount = 0;
 addRing();
 
 function addRing() {
     let tr = document.createElement("tr");
-    let
-    
+    let select = document.createElement("select");
+    let number = document.createElement("input");
+    let 
 }
