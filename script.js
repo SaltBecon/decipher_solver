133830e20afa6845ec66a3e35fe773a958f976a6
SaltBecon <163530950+SaltBecon@users.noreply.github.com>
2024-08-04 15:12:53 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -115,9 +115,9 @@ function solve() {
     });
     document.getElementById("console").innerHTML=matrix.toString();
     for (let i = 0; i < ringCount; i++) {
-        matrix = matrix.slice(0, i).concat(matrix.slice(i, ringCount).sort(function(a, b) {return b[i] - a[i]}));
-        /*
+        matrix = matrix.slice(0, i).concat(matrix.slice(i, ringCount).sort(function(a, b) {return Math.abs(b[i]) - Math.abs(a[i])}));
         matrix[i] = matrix[i].map((a) => a / matrix[i][0]);
+        /*
         for (let j = 0; j < ringCount; j++) {
             if (j = i){continue;}
             matrix[j] = matrix[j].map((a, k) => a - matrix[i][k] * matrix[j][0]);
